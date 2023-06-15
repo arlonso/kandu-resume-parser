b95ec127d9c4bd737b9e580dceae2b957ea89b44
arlonso <arlonso.glao@gmail.com>
2023-06-15 17:14:06 +0200

    add pyresparser

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+import json
+import os
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+from pyresparser import ResumeParser
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/parse', methods = ['POST'])
+def upload_file():
+      print(request.files)
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      data = ResumeParser(f.filename).get_extracted_data()
+      
+      # delete the file after parsing
+      os.remove(f.filename)
+      
+      # return stringified json object of data
+      return json.dumps(data)
